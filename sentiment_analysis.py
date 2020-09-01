ecc1dc8e88227ba259476208878589101238336d
shreyaspkl <69814100+shreyaspkl@users.noreply.github.com>
2020-09-01 13:03:17 +0530

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -15,7 +15,7 @@ y = df1.iloc[:,0].values
 import numpy as np
 Y=[0] * (len(y))
 for i in range(0,(len(y))):
-  if y[i]==1 or y[i]==2 or y[i]==3 y[i]==4:
+  if y[i]==1 or y[i]==2 or y[i]==3 or y[i]==4:
     Y[i] = 0              # Negative sentiments
   else:
     Y[i] = 1              # Positive sentiments
